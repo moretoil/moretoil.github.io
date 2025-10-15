d62492c66f0ce1e0d89f0317bf52fafbac10f3f8
moretoil <moretoil@example.com>
2025-10-15 23:35:46 +0800

    Site updated: 2025-10-15 23:35:45

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1760542444975";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1760542539639";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
